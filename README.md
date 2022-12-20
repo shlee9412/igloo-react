c992c3467ea9d0d662f0a7a05be91222ed8c615a
unknown <rlsh7321@gmail.com>
2022-12-20 12:20:24 +0900

    연락처 예제 (PUT, DELETE)

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ $> cd igloo-react
 $> npm install
 $> npm start
 ```
+
 ## 2. 목차
 - Counter - 2022. 11. 30
 - React Router - 2022. 12. 06
 - UseEffect, Controlled/Uncontrolled Component - 2022. 12. 13
-- 연락처 예제, Express 서버 추가 - 2022. 12. 14
-
+- 연락처 예제 (GET, POST), Express 서버 추가 - 2022. 12. 14
+- 연락처 예제 (PUT, DELETE) - 2022. 12. 20
 <br />
 
 **※ 백엔드 API 문서 : "npm start" 실행 후 http://localhost:8080/api/docs 로 접속**
--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -17,6 +17,7 @@ const Contacts = () => {
     phone: ''
   });
   const [contacts, setContacts] = useState<Contact[]>([]);
+  const [selectecContact, setSelectedContact] = useState<Contact | null>(null);
 
   const getAllContacts = async () => {
     try {
@@ -28,7 +29,7 @@ const Contacts = () => {
     }
   };
 
-  const createContact: React.MouseEventHandler<HTMLButtonElement> = async e => {
+  const createContact: React.FormEventHandler<HTMLFormElement> = async e => {
     e.preventDefault();
 
     if (!newContact.name) {
@@ -54,6 +55,52 @@ const Contacts = () => {
     }
   };
 
+  const deleteContact = async (e: React.MouseEvent, id: number) => {
+    const confirm = window.confirm('삭제하시겠습니까?');
+    if (!confirm) return;
+
+    try {
+      await api({
+        url: `/contacts/${id}`,
+        method: 'DELETE'
+      });
+      await getAllContacts();
+      window.alert('삭제되었습니다.');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChangeSelectedContact: React.FormEventHandler<HTMLFormElement> = e => {
+    const inputEl = e.target as HTMLInputElement;
+    // const formEl = e.currentTarget;
+
+    const selectedId = Number(inputEl.getAttribute('data-id'));
+
+    const originalContact = contacts.find(d => d.id === selectedId) || null;
+    setSelectedContact(originalContact);
+  };
+
+  const updateContact: React.FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault();
+
+    if (!selectecContact) return;
+    if (selectecContact.name === '') return window.alert('이름을 입력해주세요.');
+    if (selectecContact.phone === '') return window.alert('전화번호를 입력해주세요.');
+
+    try {
+      await api({
+        url: '/contacts',
+        method: 'PUT',
+        data: selectecContact
+      });
+      await getAllContacts();
+      window.alert('수정되었습니다.');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getAllContacts();
   }, []);
@@ -63,7 +110,7 @@ const Contacts = () => {
       <h1>Contacts</h1>
       <hr />
       <h2>연락처 추가</h2>
-      <form>
+      <form onSubmit={createContact}>
         <label htmlFor="nm">이름</label>
         <input
           type="text"
@@ -84,28 +131,65 @@ const Contacts = () => {
           }}
         />
         <br />
-        <button type="submit" onClick={createContact}>추가</button>
+        <button type="submit">추가</button>
       </form>
       <hr />
+      <h2>연락처 수정</h2>
+      {selectecContact && (
+        <form onSubmit={updateContact}>
+          <label htmlFor="nm2">이름</label>
+          <input
+            type="text"
+            id="nm2"
+            value={selectecContact.name}
+            onChange={(e) => {
+              setSelectedContact({ ...selectecContact, name: e.target.value });
+            }}
+          />
+          <br />
+          <label htmlFor="ph2">전화번호</label>
+          <input
+            type="text"
+            id="ph2"
+            value={selectecContact.phone}
+            onChange={(e) => {
+              setSelectedContact({ ...selectecContact, phone: e.target.value });
+            }}
+          />
+          <br />
+          <button type="submit">수정</button>
+        </form>
+      )}
+      <hr />
       <h2>연락처 목록</h2>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>이름</th>
-            <th>전화번호</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map(d => (
-            <tr key={d.id}>
-              <td>{d.id}</td>
-              <td>{d.name}</td>
-              <td>{d.phone}</td>
+      <form onChange={handleChangeSelectedContact}>
+        <table border={1}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID</th>
+              <th>이름</th>
+              <th>전화번호</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {contacts.map(d => (
+              <tr key={d.id}>
+                <td>
+                  <input type="radio" data-id={d.id} name="selectedContact" />
+                </td>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.phone}</td>
+                <td>
+                  <button onClick={e => deleteContact(e, d.id)}>삭제</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </form>
     </div>
   );
 };
